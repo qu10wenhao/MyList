d8e1436109301f9d3b02e956809287d5011cc76b
qu10wenhao <shark_010@163.com>
2015-06-12 20:44:53 +0800

    MyList

--- mylist.cpp
+++ mylist.cpp
@@ -1,15 +1,3 @@
-/*
- * 实现一个list类命名为MyList，其功能与python数组类似。可以用数组实现，也可以用链表实现。
- * 必须使用template <class T>，不允许使用<iostream>以外的库。
- * 需要实现的功能与测试代码见下。请务必搞清楚每一项的意义，比如什么时候需要用友元函数什么时候需要const什么时候需要&。
- * 当某些由用户正常操作可能产生的run time error（如pop一个空数组，或访问一个超出下标的元素），
- * 请使用try-catch-throw处理错误。
- * 以下类定义不保证完全正确，请根据实际需求修改，以实现样例功能为主。
- */
-
-/* 
- * 本练习不涉及到虚类和类继承，但是期末考试我就不知道了。如果有时间可以用书上的shape类来练习。
- */
 
 #include<iostream>
 
@@ -47,41 +35,39 @@ private:
 	T *a;
 	int size;
 	int pos;
-	void double_space();//数组大小不够的时候将数组大小翻倍的操作。
+	void double_space();
 public:
 	MyList(){
 		pos = 0;
 		size = 0;
 		a = new T [size];
 	}
-	MyList(int num, const T &item);//将item重复num次填入数组中。
-	MyList(const MyList<T> &l);//深复制另外一个MyList。
-    MyList(T* arr, int len);//以arr的前len个元素构造数组
+	MyList(int num, const T &item);
+	MyList(const MyList<T> &l);
+    MyList(T* arr, int len);
 
-	void push(const T &item);//将item添加在MyList最后。
-	T pop() throw(pop_error);//将MyList中最后一个元素删除，并返回这个删除的元素。
-	void insert(int index, const T &item) throw(cross_error);//将item插入到place处。
-	void clean();//清空数组。
-	int get_size();//返回MyList中元素的数量。
-	void erase(int start, int end) throw(cross_error); //删除MyList中第start到第end位的元素，包括两边。
-	T get_item(int index) throw(cross_error);//返回第index个元素。
-	MyList get_item(int start, int end) throw(cross_error);//返回MyList中第start到第end位的元素，包括两边。此处需要像python一样接受负数，具体见测试代码。
-	int count(const T &item);//返回MyList中和item相等的元素的个数。
-	void remove(const T &item);//删除MyList中第一个和item相等的元素。
+	void push(const T &item);
+	T pop() throw(pop_error);
+	void insert(int index, const T &item) throw(cross_error);
+	void clean();
+	int get_size();
+	void erase(int start, int end) throw(cross_error);
+	T get_item(int index) throw(cross_error);
+	MyList get_item(int start, int end) throw(cross_error);
+	int count(const T &item);
+	void remove(const T &item);
 
 
-	friend MyList<T> operator + <T>(const MyList<T> &l1, const MyList<T> &l2); //合并两个MyList
-    friend MyList<T> operator + <T>(const MyList<T> &l1, const T &item); //同push(T item)，但不修改l1，返回一个新数组
-	MyList<T> &operator = (const MyList<T> &l);//赋值
-	MyList<T> &operator += (const T &item);//同push(T item)
-	MyList<T> &operator += (const MyList<T> &l);//将一个MyList加入到本个MyList之后。
-	T &operator [](int index);//返回第index个元素。
-	friend std::ostream & operator<< <T>(std::ostream &os, const MyList<T> &obj);//如果T是可以直接cout的类的话（比如int），按Python数组的格式输出MyList中的每一个元素，例如：
-    // [1, 2, 5, 4, 1]
+	friend MyList<T> operator + <T>(const MyList<T> &l1, const MyList<T> &l2);
+    friend MyList<T> operator + <T>(const MyList<T> &l1, const T &item);
+	MyList<T> &operator = (const MyList<T> &l);
+	MyList<T> &operator += (const T &item);
+	MyList<T> &operator += (const MyList<T> &l);
+	T &operator [](int index);
+	friend std::ostream & operator<< <T>(std::ostream &os, const MyList<T> &obj);
 
-	void sort(bool less=true);//实现一个快速排序或归并排序，对支持比较运算符（>=<）的类进行排序。
-    // 若less=true，按照从小到大排列，否则按从大到小排列
-	void reverse();//将MyList的元素倒过来。
+	void sort(bool less=true);
+	void reverse();
 
 	~MyList(){delete [] a;}
 };
@@ -123,7 +109,7 @@ int main(){
 	a += 12; // a = [15, 4, 2, 1, 0, 12]
 	for (i=0; i<a.get_size(); ++i)
 		cout<<a[i]<<endl;
-    b = a.get_item(4, -3); // b = [] *若start > end，返回空数组
+    b = a.get_item(4, -3); // b = []
 	b = a.get_item(3, -1); // b = [1, 0, 12] 
 	a += b; // a = [15, 4, 2, 1, 0, 12, 1, 0, 12]
 	for (i=0; i<a.get_size(); ++i)
@@ -155,7 +141,7 @@ void MyList<T>::double_space(){
 }
 
 template<class T>
-MyList<T>::MyList(int num, const T &item){//将item重复num次填入数组中。
+MyList<T>::MyList(int num, const T &item){
 	size = num;
 	pos = 0;
 	a = new T [size];
